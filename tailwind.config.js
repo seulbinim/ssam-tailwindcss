35ef7d91e2079e9e59cb9323cae6023413448519
seulbinim <seulbinim@gmail.com>
2024-05-23 06:13:20 +0900

    [설정] 중단점 초기화

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,28 @@ module.exports = {
   content: ["./src/**/*.html"],
   theme: {
     fontFamily: {
-      suit: ['SUIT Variable', 'sans-serif'],
-    },
-    screens: {
-      'sm': '480px',
-      'md': '640px',
-      'lg': '768px',
-      'xl': '1024px',
+      suit: ["SUIT Variable", "sans-serif"],
     },
     extend: {
-      "colors": {
-        "primary": "#03cf5d",
-        "white": "#ffffff",
-        "warning": "#ff1414",
+      colors: {
+        primary: "#03cf5d",
+        white: "#ffffff",
+        warning: "#ff1414",
         "blue-10": "#e9f0fd",
         "gray-10": "#dadada",
         "gray-20": "#999999",
         "gray-30": "#666666",
         "gray-40": "#333333",
-        "black": "#121212",
-       },
+        black: "#121212",
+      },
+      backgroundImage: {
+        "gradient-radial":
+          "radial-gradient(circle, var(--tw-gradient-from), var(--tw-gradient-to))",
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-  ]
-}
+    // require('@tailwindcss/forms'),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
